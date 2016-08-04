6f3156351c3521b5ac447e6d832a08a71874ab12
smorineau <smorineau@gmail.com>
2016-08-04 18:34:05 +0200

    Connection string enhancements: DEFAULT_DB_CONNECT_STRING in a single file DB_CONNECT_STRING can now be a multi character string if quoted (useful for conection strings in the form "sys/sys as sysdba").

--- startup.sh
+++ startup.sh
@@ -5,19 +5,22 @@ NEW_USER=container-user
 NEW_GROUP=$NEW_USER
 NEW_UID=$(stat -c "%u" $VOLUME)
 NEW_GID=$(stat -c "%g" $VOLUME)
+DEFAULT_DB_CONNECT_STRING=utp/utp@sebora12/sebora12
 
 getent group $NEW_GID
 [ $? -gt 0 ] && groupadd --gid $NEW_GID $NEW_GROUP
 getent passwd $NEW_UID 
 [ $? -gt 0 ] && useradd -M --home $VOLUME --uid $NEW_UID --gid $NEW_GID $NEW_USER
 
+export DB_CONNECT_STRING=$DEFAULT_DB_CONNECT_STRING
+
 COMMAND=$1
 if [ "$COMMAND" == "" ]
 then
   exec su $NEW_USER
 else
-  echo "subommand $COMMAND $@"
+  #echo "subcommand $COMMAND $@"
   shift
-  echo "subommand $COMMAND $@"
+  echo "subcommand $COMMAND $@"
   exec su $NEW_USER -c "/usr/local/bin/${COMMAND} $(printf " %q" "$@")"
 fi
